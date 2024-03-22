5bc91ca76d51163cd1acaf7332931dd4ace372f7
Tricia Kim <triciapany6@gmail.com>
2024-03-22 14:30:58 +0000

    get api method

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Device;
 
 use Illuminate\Http\Request;
 
 class APIController extends Controller
 {
     //
-    function getData(){
+    public function getData(){
         return["name"=>"Tricia", "age"=>16, "email"=>"tricia@gmail.com", "address"=>"South Korea"];
     }
+
+    public function deviceList()
+    {
+        return Device::all();
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get("data",[APIController::class, 'getData']);
+Route::get('list',[APIController::class, 'deviceList']);
